16a415cc5c64e865749adf4e11e14ff78d60bdf2
Joseph Kaplin <jkaplin@e1z2r4p25.42.us.org>
2019-03-02 10:27:17 -0800

    Add a success/failure message before diff if the printout matches or not

---
+++ main.c
@@ -0,0 +1,7 @@
+
+void	ft_putstr(char const *s);
+int main()
+{
+	ft_putstr("test\n");
+	return (0);
+}
--- test.py
+++ test.py
@@ -8,15 +8,20 @@ from subprocess import call, Popen
 your_binary = argv[1]
 expected = argv[2]
 
-transfer_to_file = "./" + your_binary + " > " + "your_out"
+transfer_to_file = "./" + your_binary + " > " + ".your_out"
 os.system(transfer_to_file)
 
 if (not os.path.isfile(expected)):
-    transfer_to_file = "echo " + "\"" + expected + "\"" + " > " + "true_out"
+    transfer_to_file = "echo " + "\"" + expected + "\"" + " > " + ".true_out"
     os.system(transfer_to_file)
 else:
-    copy_expected_to_true_out_file = "cp " + expected + " true_out"
+    copy_expected_to_true_out_file = "cp " + expected + " .true_out"
     os.system(copy_expected_to_true_out_file)
+your_file = open(".your_out", "r")
+true_file = open(".true_out", "r")
+if (your_file.read() == true_file.read()):
+    print("SUCCESS")
+else:
+    print("FAILURE")
 print("                       YOUR PRINTOUT                          |                  EXPECTED PRINTOUT")
-os.system("diff -y your_out true_out")
-#diff trace
+os.system("diff -y .your_out .true_out")
